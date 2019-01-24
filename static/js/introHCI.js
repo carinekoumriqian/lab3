c564b194375f6d45024079a2388e98ca883ce971
Carine Koumriqian <ckoumriq@ucsd.edu>
2019-01-24 10:58:24 -0800

    Finished Lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,33 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Glow Cloud hailed.");
+		$("#testjs").text("Have a Nice Day!");
+		$("#testjs").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+
+	$("a.thumbnail").click(projectClick);
+
 }
+
+function projectClick(event) {
+		event.preventDefault();
+		$(this).css("background-color", "#7FFF00");
+
+		var containingProject = $(this).closest(".project");
+		
+
+		var description = $(containingProject).find(".project-description");
+
+		if(description.length == 0)
+		{
+			$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+		} 
+		else 
+		{
+			description.html("<p>Stop clicking on me! You just did it at "+(new Date()) + "</p>");
+		}
+	}
